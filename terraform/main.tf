15092ab83e9cb57b0583be67c27323f0fa358d95
krystian334 <ionutgamer55@gmail.com>
2025-07-23 17:09:15 +0300

    Modify main

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,6 @@ locals {
   prefix = "CursUnitBV"
 }
 
-variable "client_id" {
-    type      = string
-    sensitive = true
-}
-
-variable "client_secret" {
-    type      = string
-    sensitive = true
-}
-
-variable "tenant_id" {
-    type      = string
-    sensitive = true
-}
-
-variable "subscription_id" {
-    type      = string
-    sensitive = true
-}
-
 provider "azurerm" {
   features {}
 }
