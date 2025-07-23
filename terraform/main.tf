e37843b964bfb6934bd96edb451fc2da7c163dbc
krystian334 <ionutgamer55@gmail.com>
2025-07-23 15:52:58 +0300

    Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+locals {
+  prefix = "CursUnitBV"
+}
+
+variable "client_id" {
+    type      = string
+    sensitive = true
+}
+
+variable "client_secret" {
+    type      = string
+    sensitive = true
+}
+
+variable "tenant_id" {
+    type      = string
+    sensitive = true
+}
+
+variable "subscription_id" {
+    type      = string
+    sensitive = true
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.prefix}-resources"
+  location = "East US"
+}
